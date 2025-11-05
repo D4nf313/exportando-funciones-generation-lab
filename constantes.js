aa3580250df5bd92ccdac5d6bf4ca97e48c20b35
IDarwinOrtizI <sebastiancaballero0811@gmail.com>
2025-11-05 12:38:16 -0500

    Se realizo el primer punto

---
+++ constantes.js
@@ -0,0 +1,14 @@
+export const pi = 3.1416;
+export const iva = 0.19;
+export const DIAS_SEMANA = ["Lunes","Martes","Miércoles","Jueves","Viernes","Sabado","Domingo"];
+
+export function areaCirculo(radio){
+    return pi*(radio**2)
+}
+
+export function diaDeseado(dia){
+    return DIAS_SEMANA[dia-1]
+}
+
+//console.log(areaCirculo(5));
+//console.log(diaDeseado(3));
